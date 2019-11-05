20bfa1d111c06acc6de94b243156df1257764f98
la2697 <56022392+la2697@users.noreply.github.com>
2019-11-05 17:18:46 +0100

    BubbleSort

---
+++ Bubblesort/Bubblesort/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bubblesort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[5] { 5, 37, 748, 68, 4 };
+            int temp = 0;
+
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = 0; j < arr.Length; j++)
+                {
+                    if(arr[i] < arr[j])
+                    {
+                        temp = arr[i];
+
+                        arr[i] = arr[j];
+
+                        arr[j] = temp;
+                    }
+                }
+            }
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write("{0},", arr[i]);
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
